a5059908a690fd436e5d964086cfa6a1eecf44c3
houssam199 <40376744+houssam199@users.noreply.github.com>
2018-07-21 14:00:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client()
-client.on('ready' , () => {
-	console.log('iam Online');
-})
+  const Discord = require("discord.js");
+const client = new Discord.Client();
+
 client.on('message', message => {
 if (message.content === '!spam') {
       let count = 0;
